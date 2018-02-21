f76a886c4714d18191acf068fb096dffb07617cc
KaungMinKhant <phenomenalkaung@gmail.com>
2018-02-21 17:28:42 +0630

    main ta chan pee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,14 +12,125 @@
 						<br>
 					</div>
 					<div id="content1">
-						<h1>Hello</h1>
+						<p>I want to know how to attach icons to buttons in bootstrap</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+
+
 					</div>
 					<div id="content2">
-						<h1>Hello</h1>
+						<p>Go goole, Lol!</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control answer" rows="5" id="comment" placeholder="Your Answer"> </textarea>
+						<button class="btn btn-secondary btn-sm submit-button"><i class="fa fa-upload"></i> Submit</button>
 					</div>
 					<div id="nyan_sin2">
 						<br>
 					</div>
+
+				</div>
+				<div id="box">
+					<div id="nyan_sin1">
+						<br>
+					</div>
+					<div id="content1">
+						<p>I want to know how to attach icons to buttons in bootstrap</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+
+
+					</div>
+					<div id="content2">
+						<p>Go goole, Lol!</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control answer" rows="5" id="comment" placeholder="Your Answer"> </textarea>
+						<button class="btn btn-secondary btn-sm submit-button"><i class="fa fa-upload"></i> Submit</button>
+					</div>
+					<div id="nyan_sin2">
+						<br>
+					</div>
+
+				</div>
+				<div id="box">
+					<div id="nyan_sin1">
+						<br>
+					</div>
+					<div id="content1">
+						<p>I want to know how to attach icons to buttons in bootstrap</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+
+
+					</div>
+					<div id="content2">
+						<p>Go goole, Lol!</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control answer" rows="5" id="comment" placeholder="Your Answer"> </textarea>
+						<button class="btn btn-secondary btn-sm submit-button"><i class="fa fa-upload"></i> Submit</button>
+					</div>
+					<div id="nyan_sin2">
+						<br>
+					</div>
+
+				</div>
+				<div id="box">
+					<div id="nyan_sin1">
+						<br>
+					</div>
+					<div id="content1">
+						<p>I want to know how to attach icons to buttons in bootstrap</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+
+
+					</div>
+					<div id="content2">
+						<p>Go goole, Lol!</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control answer" rows="5" id="comment" placeholder="Your Answer"> </textarea>
+						<button class="btn btn-secondary btn-sm submit-button"><i class="fa fa-upload"></i> Submit</button>
+					</div>
+					<div id="nyan_sin2">
+						<br>
+					</div>
+
+				</div>
+				<div id="box">
+					<div id="nyan_sin1">
+						<br>
+					</div>
+					<div id="content1">
+						<p>I want to know how to attach icons to buttons in bootstrap</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+
+
+					</div>
+					<div id="content2">
+						<p>Go goole, Lol!</p>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-up"></i> Upvote</button>
+						<button class="btn btn-secondary btn-sm"><i class="fa fa-thumbs-down"></i> Downvote</button>
+					</div>
+					<div class="form-group">
+						<textarea class="form-control answer" rows="5" id="comment" placeholder="Your Answer"> </textarea>
+						<button class="btn btn-secondary btn-sm submit-button"><i class="fa fa-upload"></i> Submit</button>
+					</div>
+					<div id="nyan_sin2">
+						<br>
+					</div>
+
 				</div>
 			</div>
 		</div>
