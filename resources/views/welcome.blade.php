99cfeddbad31876443e028788a42849663cdd231
KaungMinKhant <phenomenalkaung@gmail.com>
2018-02-21 14:31:14 +0630

    QnA box ya b

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,31 @@
 @extends ('layouts.main')
 
 @section('main')
-<div id="topQ">
-		<div class="top_q">
-			<div class="top_q_que">
-				<p>question no 1</p>
-			</div>
-			<div class="top_q_ans">
-				<p>question no 1</p>
-			</div>
-		</div>
-		<div class="top_q">
-			<div class="top_q_que">
-				<p>question no 1</p>
-			</div>
-			<div class="top_q_ans">
-				<p>answer no 1</p>
-			</div>	
-		</div>
-		<div class="top_q">
-			<div class="top_q_que">
-				<p>question no 1</p>
-			</div>
-			<div class="top_q_ans">
-				<p>answer no 1</p>
-			</div>
-		</div>
-		<div class="top_q">
-			<div class="top_q_que">
-				<p>question no 1</p>
-			</div>
-			<div class="top_q_ans">
-				<p>answer no 1</p>
-			</div>	</div>
-			<div class="top_q">
-				<div class="top_q_que">
-					<p>question no 1</p>
-				</div>
-				<div class="top_q_ans">
-					<p>answer no 1</p>
+<div class="container">
+	<div class="row">
+
+		<div class="col-md-9 col-sm-9">
+			
+			<div class="col-md-12">
+				<div id="box">
+					<div id="nyan_sin1">
+						<br>
+					</div>
+					<div id="content1">
+						<h1>Hello</h1>
+					</div>
+					<div id="content2">
+						<h1>Hello</h1>
+					</div>
+					<div id="nyan_sin2">
+						<br>
+					</div>
 				</div>
 			</div>
 		</div>
+		<div class="col-md-3 col-sm-off">
+			<h1>Hello</h1>
+		</div>
+	</div>
+</div>
 @endsection
