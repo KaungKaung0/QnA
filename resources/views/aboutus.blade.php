34fba77bdd26735cbec0147602a4607facfbaae3
PaiHaim <dreamwalkerphh@gmail.com>
2018-02-23 15:55:19 +0630

    aboutus p

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -1,1 +1,59 @@
-<h1>Hello</h1>
+@extends('layouts.main')
+
+@section('main')
+<div class="container">
+	<br>
+	<div id="header_aboutus">
+		<h1><i class="fa fa-code"></i>Developer Portfolio</h1>
+	</div>
+	<div id="box">
+		<div id="nyan_sin1">
+			<br>
+		</div>
+		<h1>Becoming a team...</h1>
+		<div id="content1">
+			<p>We are a group of computer students. Our team is established in 2016. We are Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis quibusdam at itaque mollitia ex doloribus veniam deserunt id a, quia. Aut, numquam maiores expedita quasi id minus consectetur. Iusto, orem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis quibusdam at itaque mollitia ex doloribus veniam deserunt id a, quia. Aut, numquam maiores expedita quasi id minus consectetur. Iusto,</p>
+		</div>
+		<div id="nyan_sin2">
+			<br>
+		</div>
+	</div>
+	<div id="box">
+		<div id="nyan_sin1">
+			<br>
+		</div>
+		<h1>Our team</h1>
+		<div class="row">
+			<div class="col-md-3">
+				
+			</div>
+			<div class="col-md-9">
+				<div id="content1">
+					<table class="table">
+							<tbody>
+								<tr>
+									<th scope="row">Developer Name</th>
+									<td>-</td>
+									<td>Paing Hein Htet</td>
+								</tr>
+								<tr>
+									<th scope="row">Role</th>
+									<td>-</td>
+									<td>Front-end developer</td>
+								</tr>
+								<tr>
+									<th scope="row">Portfolio</th>
+									<td>-</td>
+									<td>I am studying Computer Science at University of Yangon. I am single.</td>
+								</tr>
+							</tbody>
+						</table>
+				</div>
+			</div>
+		</div>
+		<div id="nyan_sin2">
+			<br>
+		</div>
+	</div>
+</div>
+@endsection
