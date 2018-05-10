b933b4e563a4441a4f38f54572c3f480b413f101
PaiHaim <dreamwalkerphh@gmail.com>
2018-05-10 14:37:01 +0630

    amyargyiphyathtrtl

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,38 +1,3 @@
-<!-- nav class="navbar color-change">
-    <div id="navone">
-        <div id="leftnavone">
-            <div class="col-md-9 col-xs-11 p-l-2 p-t-2">
-                <a class="font-color" href="#sidebar" data-toggle="collapse"><i class="fa fa-navicon fa-lg"></i></a>
-            </div>
-        </div>
-        <div id="rightnavone">
-            <a class="navbar-brand font-color init" href="#">
-
-                <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-                မေးကြည့် Mayy Kyi
-            </a>
-        </div>
-    </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12 col-xs-1 p-l-0 p-r-0 collapse in" id="sidebar">
-                <div class="list-group panel">
-                    <a class="list-group-item collapsed font-color" href="{{ route('main') }}"><i class="fa fa-home"></i> <span class="hidden-sm-down">Home</span> </a>
-                    <a class="list-group-item collapsed font-color" href="{{ route('profile') }}"></i><span class="hidden-sm-down">My Profile</span></a>
-
-                    <a class="list-group-item collapsed font-color"  href="{{ route('aboutus') }}"><i class="fa fa-code"></i> <span class="hidden-sm-down">Developer Portfolio</span></a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                        {{ csrf_field() }}
-                        <button type="submit" class="btn button-official">Logout</button>
-                    </form>
-                    
-                </div>
-            </div>
-
-        </div>
-    </div>
-</nav>
- -->
 
  <nav class="navbar navbar-expand-lg default-primary-color">
   <a class="margin-fix navbar-brand text-primary-color" href="#"><img src="{{asset("img/Logo-Official.png")}}" width="120px" alt=""></a>
