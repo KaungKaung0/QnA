e4a76f7f2c1855ed65629ad337004fd5efe90618
KaungMinKhant <phenomenalkaung@gmail.com>
2018-05-13 00:18:06 +0630

    Confirmation and Search UI finished

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Question;
 use App\Answer;
-
+use App\User;
 class SearchController extends Controller
 {
     //
@@ -15,10 +15,11 @@ class SearchController extends Controller
     		$search =$request->search;
     		$found	=0;
     		$question = Question::where('question' ,  'LIKE', '%' . $search . '%')->get();
+            $u = User::select('id' , 'name' , 'profile_pic')->get();
 
     		if(($question->count()> 0)){
     			$found =1;
-    			return view('result' , compact('question' , 'found'));	
+    			return view('result' , compact('question' , 'found', 'u'));	
     		}
     		else{
     			return view('result' , compact('found'));
